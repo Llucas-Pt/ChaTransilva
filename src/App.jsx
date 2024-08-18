ed7df68d290a6037f36c9fec2930c127b3305004
unknown <Lucas-vicentte@outlook.com>
2024-08-18 19:18:05 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-
 import "./App.css";
-
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
+
+
 function App() {
   const [user, setUser] = useState(undefined);
+  
 
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
